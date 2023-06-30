367432da538abee07292b02cdccb86bbb3161b79
AchmadMahfud26 <achmadmahfud01@gmail.com>
2023-06-30 21:59:29 +0700

    1

---
+++ about.php
@@ -0,0 +1,8 @@
+<?php require ('header.php');?>
+
+<div class="content">
+    <h2>Halaman About</h2>
+    <p>Ini adalah bagian content dari halama.</p>
+</div>
+
+<?php require('footer.php');?>
---
+++ kontak.php
@@ -0,0 +1,8 @@
+<?php require ('header.php');?>
+
+<div class="content">
+    <h2>Halaman Kontak</h2>
+    <p>Ini adalah bagian content dari halama.</p>
+</div>
+
+<?php require('footer.php');?>
